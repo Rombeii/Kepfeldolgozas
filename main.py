2ab7afd82c3b3a7cbc038a5f2955fdef402e5ee8
Tamas Tardi <rombeii@freemail.hu>
2021-04-22 12:09:39 +0200

    refact

--- main.py
+++ main.py
@@ -31,19 +31,8 @@ def generate_etalon_vector(walsh_matrix):
     return etalon_vector
 
 
-if __name__ == '__main__':
-    pdf = cv.imread('resources/Calibri_test.jpg')
-    # Util.test_binarization_options(pdf)
-    Walsh_matrix = Walsh.get_matrix()
-    etalon_vector = generate_etalon_vector(Walsh_matrix)
-
-    preprocessed_image = Preprocessor.preprocess(pdf)
-
-    cv.imwrite('output/Preprocessed image_Kep.png', preprocessed_image)
-
-    letters = Segmenter.extract_letters(preprocessed_image)
-
-    predicted_letters = []
+def get_predicted_letters(letters):
+    returned_letters = []
 
     for index, letter in enumerate(letters):
         letter.resize()
@@ -73,40 +62,66 @@ if __name__ == '__main__':
             else:
                 predicted = predicted.upper()
 
-        predicted_letters.append((predicted, letter))
+        returned_letters.append((predicted, letter))
+    return returned_letters
 
-    recovered_text = []
+
+def add_whitespaces(predicted_letters, letters):
+    returned_text = []
     previous_rownum = 0
     for index, prediction in enumerate(predicted_letters):
         if prediction[1].rownum > previous_rownum and prediction[0] != '':
-            recovered_text.append("\n\n")
+            returned_text.append("\n\n")
 
         if index != 0 and letters[index].x_coord - (letters[index - 1].x_coord + letters[index - 1].width) > 20:
-            recovered_text.append(" ")
+            returned_text.append(" ")
 
-        recovered_text.append(prediction[0])
+        returned_text.append(prediction[0])
         previous_rownum = prediction[1].rownum
 
-    skipNext = False
-    clean_text = []
-    for index, text in enumerate(recovered_text):
-        if skipNext:
-            skipNext = False
+    return returned_text
+
+
+def correct_letters(letters):
+    skip_next = False
+    returned_text = []
+    for index, text in enumerate(letters):
+        if skip_next:
+            skip_next = False
             continue
-        if index != len(recovered_text) - 1 and text == '.' and recovered_text[index + 1] in ['i', '!', 'l', 'I']:
-            clean_text.append('i')
-            skipNext = True
-        elif index != len(recovered_text) - 1 and text == '\'' and recovered_text[index + 1] == '\'':
-            clean_text.append('"')
-            skipNext = True
-        elif index != len(recovered_text) - 1 and text == '?' and recovered_text[index + 1] == '.':
-            clean_text.append('"')
-            skipNext = True
-        elif index != len(recovered_text) - 1 and text == 'I' and recovered_text[index + 1] == '.':
-            clean_text.append('!')
-            skipNext = True
+        if index != len(letters) - 1 and text == '.' and letters[index + 1] in ['i', '!', 'l', 'I']:
+            returned_text.append('i')
+            skip_next = True
+        elif index != len(letters) - 1 and text == '\'' and letters[index + 1] == '\'':
+            returned_text.append('"')
+            skip_next = True
+        elif index != len(letters) - 1 and text == '?' and letters[index + 1] == '.':
+            returned_text.append('"')
+            skip_next = True
+        elif index != len(letters) - 1 and text == 'I' and letters[index + 1] == '.':
+            returned_text.append('!')
+            skip_next = True
         else:
-            clean_text.append(text)
+            returned_text.append(text)
+    return returned_text
+
+
+if __name__ == '__main__':
+    pdf = cv.imread('resources/Calibri_test.jpg')
+    # Util.test_binarization_options(pdf)
+    Walsh_matrix = Walsh.get_matrix()
+    etalon_vector = generate_etalon_vector(Walsh_matrix)
+
+    preprocessed_image = Preprocessor.preprocess(pdf)
+
+    cv.imwrite('output/Preprocessed image_Kep.png', preprocessed_image)
+
+    letters = Segmenter.extract_letters(preprocessed_image)
+
+    predicted_letters = get_predicted_letters(letters)
+    letters_with_whitespaces = add_whitespaces(predicted_letters, letters)
+    clean_text = correct_letters(letters_with_whitespaces)
+
 
     with open("output/kimenet.txt", "w") as filehandle:
         for listitem in clean_text:
